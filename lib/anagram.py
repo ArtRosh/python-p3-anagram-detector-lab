4ea75c965773c7bd9c3979bc2b0fedc8fbcce9a0
ArtRosh <artem.rosh17@gmail.com>
2025-08-19 14:24:38 -0700

    Create the class where validate words if they are angram be each other, by applying sorted function

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = sorted(word.lower())
+
+    def match(self, candidate):
+        result = []
+
+        for w in candidate:
+            lw = w.lower()
+            if sorted(lw) == self.word:
+                result.append(lw)
+        return result
+
+
+
+
+
+
